a4867b5c511342aba920ff261d5c0b2a6b6b7b9a
topcircle <topcircle@gmail.com>
2024-01-10 11:38:47 +0900

    init

---
+++ android/app/src/main/kotlin/com/example/flutter_w10_d59_explicit_animations/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_w10_d59_explicit_animations
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/d59_explicit_animations.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class D59ExplicitAnimations extends StatefulWidget {
+  const D59ExplicitAnimations({super.key});
+
+  @override
+  State<D59ExplicitAnimations> createState() => _D59ExplicitAnimationsState();
+}
+
+class _D59ExplicitAnimationsState extends State<D59ExplicitAnimations> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
